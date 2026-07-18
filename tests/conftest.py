5230f889040b323c1ecf02c967f85e51466417b9
Francis Gomes-Okoye <olukayode-gomes@hotmail.com>
2026-07-18 22:31:54 +0100

    Add conftest to create DB tables before tests

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from app.database import engine, Base
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
